fb1beb1eb00223a1fdd88d7f76a3e6d9e7e9dd89
Hubert Krajnik <167282127+HubertKr4jnik@users.noreply.github.com>
2025-11-18 19:20:17 +0100

    outdoor map 1.0 and random spawn location

---
+++ snow.js
@@ -0,0 +1,17 @@
+WA.ui.webview.open({
+  id: "snowLayer",
+  url: "/snow.html",
+  allowApi: false,
+  position: {
+    top: 0,
+    left: 0,
+  },
+  size: {
+    width: "100%",
+    height: "100%",
+  },
+  allowApi: false,
+  allowMouse: false,
+  background: "transparent",
+  fitContent: false,
+});
---
+++ tilesets/TilesetWater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TilesetWater" tilewidth="32" tileheight="32" tilecount="476" columns="28">
+ <image source="TilesetWater.png" width="896" height="544"/>
+</tileset>
