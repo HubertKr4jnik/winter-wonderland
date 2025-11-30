efd29f87dafd6d20c15576a5642ce7b703aeb405
Hubert Krajnik <167282127+HubertKr4jnik@users.noreply.github.com>
2025-12-01 00:21:14 +0100

    interior house desing and some styling

---
+++ tilesets/christmas_tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="christmas_tree" tilewidth="32" tileheight="32" tilecount="30" columns="5">
+ <image source="christmas_tree.png" width="166" height="206"/>
+</tileset>
--- tilesets/item_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="item_spritesheet" tilewidth="32" tileheight="32" tilecount="540" columns="30">
- <image source="item_spritesheet.png" width="960" height="576"/>
-</tileset>
