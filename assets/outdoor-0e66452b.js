1236adcc522c13e37d5c8b96011d15c7de483b3d
HubertKr4jnik <167282127+HubertKr4jnik@users.noreply.github.com>
2025-11-30 23:28:29 +0000

    Deploying to gh-pages from master @ efd29f87dafd6d20c15576a5642ce7b703aeb405 🚀

---
+++ assets/outdoor-0e66452b.js
@@ -0,0 +1,2 @@
+WA.onInit().then(()=>{console.log("Loading snow iframe...");const e=document.createElement("iframe");Object.assign(e.style,{position:"fixed",top:"0",left:"0",width:"100vw",height:"100vh",pointerEvents:"none",border:"none",zIndex:"2147483647"}),e.src="snow.html",document.body.appendChild(e)});
+//# sourceMappingURL=outdoor-0e66452b.js.map
